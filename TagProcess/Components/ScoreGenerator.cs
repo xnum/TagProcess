e4b24eac0dc48d98beb14f435838fb809d5fab3c
葉家郡 <s000032001@gmail.com>
2017-10-01 15:52:32 +0800

    呼叫程式列印PDF

--- TagProcess/Components/ScoreGenerator.cs
+++ TagProcess/Components/ScoreGenerator.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.IO;
 using System.Diagnostics;
+using System.Management;
+using System.Collections.Generic;
 
 namespace TagProcess.Components
 {
@@ -27,7 +26,7 @@ namespace TagProcess.Components
         public static void exportScoreToPDF(ScoreArguments args)
         {
             var doc = new Document(PageSize.A4, 3, 3, 3, 3);
-            var writer = PdfWriter.GetInstance(doc, new FileStream("score.pdf", FileMode.Create));
+            var writer = PdfWriter.GetInstance(doc, new FileStream(args.name + "score.pdf", FileMode.Create));
             string chFontPath = "c:\\windows\\fonts\\KAIU.TTF";
             BaseFont chBaseFont = BaseFont.CreateFont(chFontPath, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
             Font textFont = new Font(chBaseFont, 16);
@@ -83,7 +82,94 @@ namespace TagProcess.Components
 
             doc.Close();
 
-            Process.Start("score.pdf");
+            //Process.Start("score.pdf");
+            Print(args.name + "score.pdf");
+        }
+
+        public static void Print(string filePath)
+        {
+            //var Status = PrintJobStatus.Printing;
+            var Message = string.Empty;
+            try
+            {
+                //logger.Debug($"Printing... {filePath}");
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.Verb = "print";
+                info.FileName = filePath;
+                info.CreateNoWindow = true;
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+
+                Process p = new Process();
+                p.StartInfo = info;
+                p.Start();
+
+                p.WaitForInputIdle();
+                //以下邏輯克服無法得知Acrobat Reader或Foxit Reader是否列印完成的問題
+                //最多等待180秒（假設所有檔案可在3分鐘內印完）
+                var timeOut = DateTime.Now.AddSeconds(180);
+                bool printing = false; //是否開始列印
+                bool done = false; //是否列印完成
+                                   //取純檔名部分，跟PrintQueue進行比對
+                string pureFileName = Path.GetFileName(filePath);
+                //限定最大等待時間
+                while (DateTime.Now.CompareTo(timeOut) < 0)
+                {
+                    if (!printing)
+                    {
+                        //未開始列印前發現檔名相同的列印工作
+                        if (CheckPrintQueue(pureFileName))
+                        {
+                            printing = true;
+                            Console.WriteLine($"[{pureFileName}]列印中...");
+                        }
+                    }
+                    else
+                    {
+                        //已開始列印後，同檔名列印工作消失表示列印完成
+                        if (!CheckPrintQueue(pureFileName))
+                        {
+                            done = true;
+                            Console.WriteLine($"[{pureFileName}]列印完成");
+                            break;
+                        }
+                    }
+                    System.Threading.Thread.Sleep(100);
+                }
+                try
+                {
+                    //若程序尚未關閉，強制關閉之
+                    if (false == p.CloseMainWindow())
+                        p.Kill();
+                }
+                catch
+                {
+                }
+                if (!done)
+                {
+                    Console.WriteLine($"無法確認報表[{pureFileName}]列印狀態！");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {DateTime.Now:HH:mm:ss} {ex.Message}");
+            }
+        }
+
+        //需查詢 WMI 記得加入參照及 using System.Management; 
+        private static bool CheckPrintQueue(string file)
+        {
+            //尋找PrintQueue有沒有檔案相同的列印工作
+            string searchQuery =
+                "SELECT * FROM Win32_PrintJob";
+            var printJobs =
+                     new ManagementObjectSearcher(searchQuery).Get();
+            foreach(ManagementObject job in printJobs)
+            {
+                if ((string)job.Properties["Document"].Value == file)
+                    return true;
+            }
+            return false;
+            //return printJobs.Any(o => (string)o.Properties["Document"].Value == file);
         }
     }
 }
